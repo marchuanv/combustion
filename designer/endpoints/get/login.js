d8c9d3fba6291e246aff472d2766ee3184193a44
apptesting <marchuanv@sahomeloans.com>
2022-06-06 12:52:39 +0200

    refactor

--- designer/endpoints/get/login.js
+++ designer/endpoints/get/login.js
@@ -1,3 +1,6 @@
+const path = require("path");
+const fs = require("fs");
+const loginViewPath = path.join(`../../${__dirname}`, 'views', 'login.html');
 module.exports = {
     response: {
         statusCode: null,
@@ -11,13 +14,10 @@ module.exports = {
     },
     handle: async () => {
         try {
-            const path = require("path");
-            const fs = require("fs");
-            const loginHTMLPath = path.resolve(`${__dirname}/content/login.html`);
             module.exports.response.headers["Content-Type"] = "text/html";
             module.exports.response.statusCode = 200;
             module.exports.response.statusMessage = "success";
-            module.exports.response.data = fs.readFileSync(loginHTMLPath,"UTF-8");
+            module.exports.response.data = fs.readFileSync(loginViewPath,"UTF-8");
         } catch (err) {
             module.exports.response.data = "";
             module.exports.response.statusCode = 500;
